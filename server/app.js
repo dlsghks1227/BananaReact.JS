5c319d5e75b7a0a56c3cece55b011e680c57babf
Banana <lh1227@yahoo.co.kr>
2020-03-21 00:50:52 +0900

    Modify package.js

--- server/app.js
+++ server/app.js
@@ -1,51 +1,35 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-
 // 요청된 쿠키를 쉽게 추출할 수 있도록 도와주는 미들웨어
 var cookieParser = require('cookie-parser');
-
-// 로그 기록을 남기는 morgan모듈
+// 로그 기록을 남기는 모듈
 var logger = require('morgan');
 
+// -----Router-----
 var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/api/users');
+var usersRouter = require('./routes/users');
+// ----------------
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 
 // body-parser를 사용
 app.use(express.json());
+
 // extended
 //  false : 노드의 query-string 모듈을 사용하여 쿼리스트링을 해석
 //  true  : qs모듈을 사용하여 쿼리스트링을 해석
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
+
 // Express에서 제공되고 정적 파일 서비스를 도와준다
 app.use(express.static(path.join(__dirname, 'public')));
 
+// -----Router-----
 app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use('/users', usersRouter);
+// ----------------
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,28 @@
 var express = require('express');
-
 var router = express.Router();
+
+const Account = [
+  { email: 'admin@test.com', password:'123', name:'banana', id:'admin'}
+]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 router.post('/login', function(req, res, next) {
-  console.log(req.body.email);
-  console.log(req.body.password);
+  if(typeof req.body.password !== "string") {
+    return res.status(401).json({
+      error:"LOGIN FAILED",
+      code: 1,
+    })
+  }
+
+  const user = Account.find(user => user.email === req.body.email && user.password === req.body.password);
+  if(user === undefined) return res.json({success: false});
+
+  console.log(user);
+  //var usernameRegex = /^[a-z0-9]+$/;
   
   return res.json({success: true});
 });
